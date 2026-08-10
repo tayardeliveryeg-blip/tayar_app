1f0e4448f946f69bab8d8594a44e0b2332c34255
T2 <tayardeliveryeg@gmail.com>
2026-08-10 13:36:16 +0300

    chore: حذف كود ميت من محاولة تقسيم الشاشات المتوقفة (home/order_confirmation/searching_offers/trip_tracking)

--- lib/screens/passenger/home/models/nearby_driver.dart
+++
@@ -1,9 +0,0 @@
-import 'package:latlong2/latlong.dart';
-
-class NearbyDriver {
-  LatLng displayed;
-  LatLng prev;
-  LatLng target;
-
-  NearbyDriver({required this.displayed, required this.prev, required this.target});
-}
--- lib/screens/passenger/searching_offers/models/nearby_driver_marker.dart
+++
@@ -1,9 +0,0 @@
-import 'package:latlong2/latlong.dart';
-
-class NearbyDriverMarker {
-  LatLng displayed;
-  LatLng prev;
-  LatLng target;
-
-  NearbyDriverMarker({required this.displayed, required this.prev, required this.target});
-}
